a25f83a17ea11748cd93c3e914d4bf1bee98dbf5
Harshal Naik <harshalnaik2020@gmail.com>
2025-08-10 14:08:26 +0530

    Update and rename R assignment 2024.R to cachematrix.R

---
+++ cachematrix.R
@@ -0,0 +1,35 @@
+## Function to create a special matrix object that can cache its inverse
+makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  
+  get <- function() x
+  
+  setInverse <- function(inverse) inv <<- inverse
+  
+  getInverse <- function() inv
+  
+  list(set = set,
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
+}
+
+## Function to compute the inverse of the matrix returned by makeCacheMatrix
+cacheSolve <- function(x, ...) {
+  inv <- x$getInverse()
+  
+  if (!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setInverse(inv)
+  inv
+}
